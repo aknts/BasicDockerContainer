11564690c6102bb9b8d2f04e9004330bfc2c4829
aknts <25027425+aknts@users.noreply.github.com>
2020-06-21 09:44:39 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+
 if [ ! -d $appmodule ]; then
-	git clone https://github.com/aknts/$appmodule
-	mkdir $appmodule/logs
-	npm install
-	cat > config.js << EOL
-		var config = $appconfig;
-		module.exports = config;
+        git clone https://github.com/aknts/$appmodule
+        cd $appmodule
+        mkdir logs
+        touch logs/$appmodule_$(date +%Y-%m-%d.%H:%M:%S).out
+        touch logs/$appmodule_$(date +%Y-%m-%d.%H:%M:%S).err
+        cat > config.js << EOL
+var config = $appconfig;
+module.exports = config;
 EOL
-	touch "logs/$appmodule_$(date +%Y-%m-%d.%H:%M:%S).out"
-	touch "logs/$appmodule_$(date +%Y-%m-%d.%H:%M:%S).err"
-	exec $1 $appmodule/$2
+        npm install
+        exec $1 $2
 fi
